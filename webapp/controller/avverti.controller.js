ec5e6d894232b3206b64db25142c05d3f228f0e1
LuigiAdamache <luigiadamache11@gmail.com>
2021-06-22 10:44:43 +0000

    AggiuntoDialog

--- webapp/controller/avverti.controller.js
+++ webapp/controller/avverti.controller.js
@@ -104,6 +104,29 @@ sap.ui.define([
                     return MessageBox.error("Nessun fornitore associato")
                 }  
               
+            },
+        openQuickView: function (oEvent, oModel) {
+			var oButton = oEvent.getSource(),
+				oView = this.getView();
+
+			if (!this._pQuickView) {
+				this._pQuickView = Fragment.load({
+					id: oView.getId(),
+					name: "suppliers.suppliers.fragment.Quick",
+					controller: this
+				}).then(function (oQuickView) {
+					oView.addDependent(oQuickView);
+					return oQuickView;
+				});
+			}
+			this._pQuickView.then(function (oQuickView){
+				oQuickView.setModel(oModel);
+				oQuickView.openBy(oButton);
+			});
+		},
+            onQuick: function (oEvent) {
+            var oModel = this.getView().getModel();
+			this.openQuickView(oEvent, oModel);
             }
         });
     });
